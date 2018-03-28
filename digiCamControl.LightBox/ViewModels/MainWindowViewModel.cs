303f5934f68ba76964751a9e72d6645932ab1237
Duka Istvan <dukusi@gmail.com>
2018-03-28 10:21:18 +0300

    Initial commit

---
+++ digiCamControl.LightBox/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Controls;
+using GalaSoft.MvvmLight;
+
+namespace digiCamControl.LightBox.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private ContentControl _contentControl;
+
+        public ContentControl ContentControl
+        {
+            get { return _contentControl; }
+            set
+            {
+                _contentControl = value;
+                RaisePropertyChanged(() => ContentControl);
+            }
+        }
+
+
+    }
+}
